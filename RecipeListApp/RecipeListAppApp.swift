ddd67f08bfdd3609dd285c455bcb1995e83af5e1
Piyush <piyush888@gmail.com>
2021-09-01 17:04:51 +0530

    Initial Commit

---
+++ RecipeListApp/RecipeListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListAppApp.swift
+//  RecipeListApp
+//
+//  Created by Piyush Garg on 01/09/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
